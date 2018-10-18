5f2b62f6c72eecf2cbad896d9784fca3c880060c
Michael Ozoemena <michaelozoemena2@gmail.com>
2018-10-18 15:51:42 +0100

    Create dummy InputDropdown component

---
+++ src/containers/courses/InputDropdown.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class InputDropdown extends Component {
+  render = () => {
+    return (
+      <select className="form-control">
+        <option>Michael Ozoemena</option>
+      </select>
+    )
+  }
+}
+
+export default InputDropdown
