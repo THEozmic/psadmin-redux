6d9a65ede75156dfb8271399e364b9834287a3ed
Michael Ozoemena <michaelozoemena2@gmail.com>
2018-10-18 15:27:01 +0100

    Update routing

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route, NavLink, Redirect } from 'react-router-dom'
 import AuthorsPage from '../authors/AuthorsPage'
+import CoursesPage from '../courses/CoursesPage'
 import About from '../about'
 
 const App = () => (
   <div>
+    <nav className="p-3">
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/authors">
+            Authors
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/courses">
+            Courses
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
     <main>
-      <Route exact path="/" component={AuthorsPage} />
-      <Route exact path="/about-us" component={About} />
+      <Route exact path="/authors" component={AuthorsPage} />
+      <Route exact path="/courses" component={CoursesPage} />
+      <Redirect from="/" to="/authors" />
     </main>
   </div>
 )
